dc6602d3d2b0b4c75d6d0feed97302befb209a7f
seahsz97 <seahsz@gmail.com>
2025-03-01 20:25:36 +0800

    Initial commit

---
+++ server/src/main/java/vttp/csf/day35workshopExtra/Day35workshopExtraApplication.java
@@ -0,0 +1,13 @@
+package vttp.csf.day35workshopExtra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day35workshopExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day35workshopExtraApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/vttp/csf/day35workshopExtra/Day35workshopExtraApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.csf.day35workshopExtra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day35workshopExtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
